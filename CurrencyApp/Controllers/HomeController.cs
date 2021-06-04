a3b0f6ff04fb475eb3c202f07a336af3b2cf75fd
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-04 19:29:38 +0300

    Вывод курса доллара и евро на представлении

--- CurrencyApp/Controllers/HomeController.cs
+++ CurrencyApp/Controllers/HomeController.cs
@@ -1,26 +1,30 @@
-﻿using CurrencyApp.Models;
+﻿using System;
+using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Threading.Tasks;
+using CurrencyApp.Models;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace CurrencyApp.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly IMemoryCache memoryCache;
         private readonly ILogger<HomeController> _logger;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IMemoryCache memoryCache)
         {
             _logger = logger;
+            this.memoryCache = memoryCache;
         }
 
         public IActionResult Index()
         {
-            return View();
+            if (!memoryCache.TryGetValue("key_currency", out CurrencyConverter model))
+            {
+                throw new Exception("Ошибка получения данных");
+            }
+            return View(model);
         }
 
         public IActionResult Privacy()
