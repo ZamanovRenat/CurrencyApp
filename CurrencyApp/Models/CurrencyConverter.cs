6d995a315a2a24fe7f87912af5568868da05360f
ZamanovRenat <zamanov.renat@mail.ru>
2021-06-04 18:43:25 +0300

    Модель курса валют в рублях

---
+++ CurrencyApp/Models/CurrencyConverter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CurrencyApp.Models
+{
+    public class CurrencyConverter
+    {
+        public decimal USD { get; set; }
+        public decimal ConvertToUSD(decimal priceRUB) => priceRUB / USD;
+
+        public decimal EUR { get; set; }
+        public decimal ConvertToEUR(decimal priceRUB) => priceRUB / EUR;
+    }
+}
